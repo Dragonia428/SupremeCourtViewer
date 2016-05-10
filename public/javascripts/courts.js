f885805c6c3190965e3ec09e6ec32869b25d3040
Irving Derin <Irving.Derin@gmail.com>
2016-05-10 13:48:27 -0400

    Setting up the front page

--- public/javascripts/courts.js
+++ public/javascripts/courts.js
@@ -2,6 +2,22 @@
  * Created by scarzer on 5/10/16.
  */
 console.log("Hello World");
-$.get('/api/node/100000', (data) =>{
-    console.log(data);
+var container = document.getElementById('networkGraphColumn');
+var nodes = new vis.DataSet();
+var edges = new vis.DataSet();
+var data = {
+    nodes : nodes,
+    edges : edges
+};
+var options = {interaction : {hover:true}};
+var network = new vis.Network(container, data, options);
+
+$.get('/api/node/100000', function (data){
+    console.log(data['node']['n']);
+    var node = data['node']['n'];
+    nodes.add({
+        id: node.properties['file'],
+        label: node.properties['case_name']
+    });
+    console.log(nodes);
 });
